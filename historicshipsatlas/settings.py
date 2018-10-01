f6770865c924fe72642e2c32902ebf79a1a6edc3
Doug Newman <doug@dougnewman.org>
2018-10-01 02:25:54 -0400

    Registered app and specified database

--- historicshipsatlas/settings.py
+++ historicshipsatlas/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'atlas.apps.AtlasConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'historicshipsatlas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'historicshipsatlas',
+        'USER': 'historicshipsatlas',
+        'PASSWORD': 'HSADBpw1018!',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
