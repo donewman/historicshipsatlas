f12245a43c9ab2662d6a82ed707cf1c5d908abf6
Doug Newman <doug@dougnewman.org>
2018-10-01 18:16:45 -0400

    Fixed error with index view.

--- atlas/views.py
+++ atlas/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from atlas.models import Ship
 
+def index(request):
+    return render(request, 'index.html')    
+
 class ShipDetailView(generic.DetailView):
     model = Ship
