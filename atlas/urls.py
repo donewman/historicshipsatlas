9e4032a4ab536444e2cd739682937187e9bb85e0
Doug Newman <doug@dougnewman.org>
2018-10-01 18:07:35 -0400

    Created basic generic, index, and ship details views

--- atlas/urls.py
+++ atlas/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from atlas import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('ship/<int:pk>', views.ShipDetailView.as_view(), name='ship-detail'),
 ]
