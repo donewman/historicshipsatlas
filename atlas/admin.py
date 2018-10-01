1f3a31815a2bd0c567dcc247357fd2464d44d12a
Doug Newman <doug@dougnewman.org>
2018-10-01 02:40:45 -0400

    Registered models

--- atlas/admin.py
+++ atlas/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from atlas.models import Flag, Type, Builder, City, Country, Register, Use, Status, Owner, Ship
+
+admin.site.register(Flag)
+admin.site.register(Type)
+admin.site.register(Builder)
+admin.site.register(City)
+admin.site.register(Country)
+admin.site.register(Register)
+admin.site.register(Use)
+admin.site.register(Status)
+admin.site.register(Owner)
+admin.site.register(Ship)
