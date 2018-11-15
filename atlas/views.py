44f1f58ff7f1168158c06daea30a6331b9eae80f
Doug Newman <doug@dougnewman.org>
2018-11-15 02:57:27 -0500

    Added static ships to map in index view

--- atlas/views.py
+++ atlas/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
+from django.core import serializers
 from atlas.models import Ship
 
 def index(request):
-    return render(request, "index.html",)
+    static_ships = serializers.serialize("json", Ship.objects.all().filter(fleetmon__isnull=True))
+    context = {"static_ships": static_ships,}
+    return render(request, "index.html", context,)
 
 class ShipDetailView(generic.DetailView):
     model = Ship
